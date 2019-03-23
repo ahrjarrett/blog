c7d70c3cd066a1153272064a358817de1d45eba7
Andrew Jarrett <ahrjarrett@gmail.com>
2019-03-23 01:01:25 -0500

    capitalize alltagsidndex and singletagindex for linux/git issue

---
+++ src/templates/SingleTagIndex.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
+
+const SingleTagTemplate = ({ data, pageContext, location }) => {
+  const { posts, tagName } = pageContext
+
+  return (
+    <Layout location={location}>
+      <div style={{ marginBottom: "20px" }}>Other posts about {tagName}:</div>
+      {posts.map(post => {
+        return (
+          <li key={post.frontmatter.path}>
+            <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+          </li>
+        )
+      })}
+    </Layout>
+  )
+}
+
+export default SingleTagTemplate
