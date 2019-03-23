4dce8c7970cf7239bd201791c362e461d1e76cc5
Andrew Jarrett <ahrjarrett@gmail.com>
2019-03-22 23:26:55 -0500

    update gatsby config to not use pathPrefix, pass Layout location prop

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
       `
     }
   },
-  pathPrefix: `/test`,
+  //pathPrefix: `/test`,
   plugins: [
     `gatsby-plugin-typescript`,
     `gatsby-plugin-react-helmet`,
--- src/templates/singleTagIndex.js
+++ src/templates/singleTagIndex.js
@@ -1,29 +1,22 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import Layout from '../components/Layout'
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
 
-const SingleTagTemplate = ({ data, pageContext }) => {
+const SingleTagTemplate = ({ data, pageContext, location }) => {
   const { posts, tagName } = pageContext
 
   return (
-    <Layout>
-      <div style={{ marginBottom: '20px' }}>
-        Other posts about {tagName}:
-      </div>
-      {
-        posts.map(post => {
-          return (
-            <li key={post.frontmatter.path}>
-              <Link to={post.frontmatter.path}>
-                {post.frontmatter.title}
-              </Link>
-            </li>
-          )
-        })
-      }
-    </Layout >
+    <Layout location={location}>
+      <div style={{ marginBottom: "20px" }}>Other posts about {tagName}:</div>
+      {posts.map(post => {
+        return (
+          <li key={post.frontmatter.path}>
+            <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+          </li>
+        )
+      })}
+    </Layout>
   )
 }
 
 export default SingleTagTemplate
-
