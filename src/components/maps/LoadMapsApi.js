3530f7260aad3b887b5b0849901cea8479e3c2c6
Andrew Jarrett <ahrjarrett@gmail.com>
2019-03-24 18:45:04 -0500

    convert LoadMapsApi to use hooks

--- src/components/maps/LoadMapsApi.js
+++ src/components/maps/LoadMapsApi.js
@@ -1,11 +1,33 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { loadScript } from "../../utils/loadScript"
 
-const getMapsUri = key => `https://maps.googleapis.com/maps/api/js?key=${key}`
 const { GATSBY_MAPS_KEY } = process.env
 const scriptId = "googleMapsScript"
+const getMapsUri = key => `https://maps.googleapis.com/maps/api/js?key=${key}`
 
-class LoadMapsApi extends React.Component {
+function LoadMapsApi() {
+  let _script
+  const [apiLoaded, markApiAsLoaded] = useState(false)
+  const loadApi = () =>
+    loadScript(getMapsUri(GATSBY_MAPS_KEY), scriptId)
+      .then(() => {
+        _script = document.getElementById(scriptId)
+        markApiAsLoaded({ apiLoaded: true })
+      })
+      .catch(() => console.log("ERROR LOADING MAPS API"))
+
+  useEffect(() => {
+    loadApi()
+    return () => {
+      _script && _script.parentNode.removeChild(_script)
+      window.google = null
+    }
+  }, []) // empty array
+
+  return null
+}
+
+class LoadMapsApiClass extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
