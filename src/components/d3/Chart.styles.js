0543cd2b7771c823bcb241e7a490bf9c2fc1aea4
Andrew Jarrett <ahrjarrett@gmail.com>
2019-03-25 09:12:41 -0500

    chart blog is coming along :tada:

---
+++ src/components/d3/Chart.styles.js
@@ -0,0 +1,86 @@
+import styled, { css } from "styled-components"
+
+export const ChartStyles = styled.div`
+  display: flex;
+  justify-content: center;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 0.625rem 0px;
+  margin-top: 1.875rem;
+  margin-bottom: 2.725rem;
+`
+
+// export const ChartStyles = styled.div`
+//   visibility: visible;
+//   display: flex;
+//   justify-content: center;
+//   box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 0.625rem 0px;
+
+//   text {
+//     font-size: 12px;
+//     stroke: none;
+//     fill: #999;
+//   }
+
+//   text.crossBarText {
+//     fill: #666;
+//     width: 200px;
+//   }
+
+//   path.domain {
+//     fill: none;
+//     stroke: #aaa;
+//   }
+
+//   g.tick {
+//     line {
+//       color: transparent;
+//       stroke: #eee;
+//       stroke-opacity: 1;
+//     }
+//   }
+
+//   .crossBar line {
+//     stroke: #333;
+//     stroke-width: 1px;
+//     pointer-events: none;
+//     shape-rendering: crispEdges;
+//   }
+
+//   .chartOverlay {
+//     fill: none;
+//     pointer-events: all;
+//   }
+
+//   .infoBox rect {
+//     stroke: #ccccd1;
+//     pointer-events: none;
+//     stroke-width: 1px;
+//     shape-rendering: crispEdges;
+//     font-size: 11px;
+//     fill: #fff;
+//     fill-opacity: 0.9;
+//   }
+
+//   tspan,
+//   text.crossBarText {
+//     font-size: 15px;
+//   }
+
+//   tspan {
+//     fill: #2d2d32;
+//   }
+
+//   .infoBoxElevationValue,
+//   .infoBoxGradeValue {
+//     font-weight: 600;
+//   }
+
+//   .elevationChartGrid line {
+//     stroke: lightgrey;
+//     stroke-opacity: 0.7;
+//     shape-rendering: crispEdges;
+//   }
+
+//   .elevationChartGrid path {
+//     stroke-width: 0;
+//   }
+// `
